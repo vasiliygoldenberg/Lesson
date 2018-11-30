c9517a00ad4857d5810e8524f4d57a58584cc586
Vasiliy Goldenberg <vasiliy.goldenberg@gmail.com>
2018-12-01 00:12:27 +0300

    Class Car

---
+++ Lesson/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class Car
+    {
+        public  readonly int maxSpeed;
+        private int currSpeed;
+
+        public Car(int max)
+        {
+            maxSpeed = max;
+        }
+        public Car()
+        {
+            currSpeed = 55;
+        }
+        public int Speed
+        {
+            get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+                if (currSpeed > maxSpeed)
+                {
+                    currSpeed = maxSpeed;
+                }
+            }
+        }
+
+    }
+}
--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -10,6 +10,12 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car(80);
+
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+
+            Console.ReadLine();
         }
     }
 }
